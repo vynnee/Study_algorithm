0b7bba249652a19861dfeefd575e0f65f2467131
vynnee <64735927+vynnee@users.noreply.github.com>
2021-09-02 17:32:54 +0900

    Add files via upload

---
+++ baekjoon_10809.cc
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    char s[100] = { 0 };
+    
+    scanf("%s", &s);
+    
+    for(int i = 97; i <= 122; i++){
+        int j = 0;
+        while(s[j] != 0){
+            if(s[j] == (char)i)break;
+            j++;
+        }
+        if(s[j] == (char)i)printf("%d ", j);
+        else printf("-1 ");
+    }
+}
