4e6e51cfc3acfd6876ece44d5aa965aa18121836
vynnee <64735927+vynnee@users.noreply.github.com>
2021-09-02 15:23:14 +0900

    Add files via upload

---
+++ programmers_가장큰정사각형찾기.c
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int dp[1001][1001] = {0};
+
+int solution(vector<vector<int>> board)
+{
+    int ans = 0;
+    int row = board.size();
+    int col = board[0].size();
+    for (int i = 1; i <= row; ++i)
+    {
+        for (int j = 1; j <= col; ++j)
+        {
+            if(board[i-1][j-1] != 0 )
+            {
+                dp[i][j] = min(dp[i][j-1], min(dp[i-1][j], dp[i-1][j-1])) + 1;
+                ans = max(ans, dp[i][j]);
+            }
+        }
+    }
+    return ans*ans;
+}
