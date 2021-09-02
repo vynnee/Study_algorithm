b825fe5a6a2dab46bca1986f8a7f743394a263f3
vynnee <64735927+vynnee@users.noreply.github.com>
2021-09-02 21:54:35 +0900

    Add files via upload

---
+++ programmers_음양더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+    
+    for(int i = 0; i < absolutes.size(); i++) {
+        if(signs[i] == false)
+            absolutes[i] = absolutes[i] * -1;
+        answer += absolutes[i];
+    }
+    
+    return answer;
+}
